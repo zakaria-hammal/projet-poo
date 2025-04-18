b227a7c14c52d319b8149b74ef1b3234de30a059
zakaria-hammal <hammalzakaria06@gmail.com>
2025-04-18 21:58:01 +0100

    Improving Utilisateur Class

--- Profil.java
+++ Profil.java
@@ -1,3 +1,17 @@
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Profil {
-    
+    private Status status;
+    private Point itenairairePassager;
+    private ArrayList<Point> itenairaireChauffeur;
+    private ArrayList<LocalDate>[] horaires;
+
+    public Profil(Status stat, Point itPass, ArrayList<Point> itChauf, ArrayList<LocalDate>[] hor) {
+        this.status = stat;
+        this.itenairairePassager = itPass;
+        this.itenairaireChauffeur = itChauf;
+        this.horaires = hor;
+    }
 }
--- Utilisateur.java
+++ Utilisateur.java
@@ -1,8 +1,12 @@
-public class Utilisateur {
+
+import java.io.Serializable;
+
+public class Utilisateur implements Serializable {
     private String nom;
     private String prenom;
     private int matricule;
     private double reputation = 2.5;
+    private Profil profil;
 
     public Utilisateur(String nom, String prenom, int mat, double rep) {
         this.nom = nom;
@@ -11,7 +15,7 @@ public class Utilisateur {
         this.reputation = rep;
     }
 
-    // Getters :
+    // Getteurs :
     public String getNom() {
         return this.nom;
     }
@@ -28,7 +32,7 @@ public class Utilisateur {
         return this.reputation;
     }
 
-    // Setters :
+    // Setteurs :
     public void setNom(String nom) {
         this.nom = nom;
     }
@@ -44,4 +48,20 @@ public class Utilisateur {
     public void setReputation(int rep) {
         this.reputation = rep;
     }
+
+    // Fin des accesseurs
+
+    /* à terminer
+    public static Utilisateur login() {
+        ObjectOutputStream out;
+
+        try {
+            FileOutputStream fos = new FileOutputStream("FichiersDeSauvgarde/fichiersATS");    
+            out = new ObjectOutputStream(fos);
+        } catch (Exception e) {
+            System.out.println("Erreur Systeme");
+            return null;
+        }
+    }
+    */
 }
