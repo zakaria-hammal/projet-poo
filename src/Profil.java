635af1ef7f50c5b220a5f7bc5e617e3f6d5a5d69
zakaria-hammal <hammalzakaria06@gmail.com>
2025-04-25 22:59:57 +0100

    Adding setters and getters for the Profil

--- src/Profil.java
+++ src/Profil.java
@@ -14,4 +14,42 @@ public class Profil {
         this.itenairaireChauffeur = itChauf;
         this.horaires = hor;
     }
+
+    // getteurs :
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Point getItenairairePassager() {
+        return itenairairePassager;
+    }
+
+    public ArrayList<Point> getItenairaireChauffeur() {
+        return itenairaireChauffeur;
+    }
+
+    public ArrayList<LocalDate>[] getHoraires() {
+        return horaires;
+    }
+
+    // setteurs :
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setItenairairePassager(Point itenairairePassager) {
+        this.itenairairePassager = itenairairePassager;
+    }
+
+    public void setItenairaireChauffeur(ArrayList<Point> itenairaireChauffeur) {
+        this.itenairaireChauffeur = itenairaireChauffeur;
+    }
+
+    public void setHoraires(ArrayList<LocalDate>[] horaires) {
+        this.horaires = horaires;
+    }
+
+    
 }
