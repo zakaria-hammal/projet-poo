985449008c7566bb6271e0496a3e2165231c2c99
meowgrabber <bwassim713@gmail.com>
2025-05-05 11:05:06 +0100

    Update Etudiant.java
    Reconstruction

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -4,7 +4,7 @@ public class Etudiant extends Utilisateur implements Serializable {
     LocalDate anneeAdmistion;
     Faculte fac;
     String specialite;
-
+// Constructeur
     public Etudiant(String nom, String prenom, String password, String mat,
                     double repC, double repP, LocalDate anneeAdmistion, Faculte fac, String specialite,)
             throws MatriculeException, ReputationException {
@@ -13,6 +13,7 @@ public class Etudiant extends Utilisateur implements Serializable {
         this.specialite = specialite;
         this.fac=fac;
     }
+    // Getteurs
     public String getSpecialite() {
         return specialite;
     }
@@ -20,47 +21,34 @@ public class Etudiant extends Utilisateur implements Serializable {
     public LocalDate getAnneeAdmistion() {
         return anneeAdmistion;
     }
-    public void getFaculteEt(int choix) {
-        System.out.println("Inserez votre Faculte: " + "1)Math,\n" +
+    public Faculte getFaculteEn(int choix) {
+        System.out.println("Inserez votre Faculte: " +
+                "1)Math,\n" +
                 "2)Informatique,\n" +
                 "3)Physique,\n" +
                 "4)Chimie,\n" +
                 "5)Biologie,\n" +
-                " 6)GénieC,\n" + "7)GénieE,\n" +
+                "6)GénieC,\n" +
+                "7)GénieE,\n" +
                 "8)GénieMP,\n" +
                 "9)Géologie");
-        if (choix >= 1 && choix <= 9) {
-            switch (choix) {
-                case 1:
-                    this.fac = Faculte.Math;
-                    break;
-                case 2:
-                    this.fac = Faculte.Informatique;
-                    break;
-                case 3:
-                    this.fac = Faculte.Physique;
-                    break;
-                case 4:
-                    this.fac = Faculte.Chimie;
-                    break;
-                case 5:
-                    this.fac = Faculte.Biologie;
-                    break;
-                case 6:
-                    this.fac = Faculte.GénieC;
-                    break;
-                case 7:
-                    this.fac = Faculte.GénieE;
-                    break;
-                case 8:
-                    this.fac = Faculte.GénieMP;
-                    break;
-                case 9:
-                    this.fac = Faculte.Géologie;
-                    break;
-            }
-        } else System.out.println("Erreur");
+
+        switch (choix) {
+            case 1: return Faculte.Math;
+            case 2: return Faculte.Informatique;
+            case 3: return Faculte.Physique;
+            case 4: return Faculte.Chimie;
+            case 5: return Faculte.Biologie;
+            case 6: return Faculte.GénieC;
+            case 7: return Faculte.GénieE;
+            case 8: return Faculte.GénieMP;
+            case 9: return Faculte.Géologie;
+            default:
+                System.out.println("Erreur");
+                return null;
+        }
     }
+    // Setteurs
     public void setAnneeAdmistion(LocalDate anneeAdmistion) {
         this.anneeAdmistion = anneeAdmistion;
     }
@@ -72,6 +60,7 @@ public class Etudiant extends Utilisateur implements Serializable {
     public void setFac(Faculte fac) {
         this.fac = fac;
     }
+    // Methode responsable d'inscription des etudiants dans l'app.
     public static void RajouterEtudiant(String filePath, Etudiant x) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath, true))) {
             out.writeObject(x);
@@ -80,20 +69,4 @@ public class Etudiant extends Utilisateur implements Serializable {
             System.err.println("Une erreur est survenue lors de l'écriture de l'objet : " + e.getMessage());
         }
     }
-    public static boolean VerifierEtudiant(String filePath, Etudiant x) {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
-            while (true) {
-                Etudiant e = (Etudiant) in.readObject();
-                if (e.getMatricule().equals(x.getMatricule()) && e.getpassword().equals(x.getpassword())) {
-                    return true;
-                }
-            }
-        } catch (EOFException e) {
-            // End of file reached — no match found
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Erreur lors de la lecture : " + e.getMessage());
-        }
-        return false;
     }
-
-}
