fe9e4c3d27186f9b5c260037104a7c94e20b5eb5
meowgrabber <bwassim713@gmail.com>
2025-05-05 19:17:01 +0100

    Update Enseignant.java

--- src/Enseignant.java
+++ src/Enseignant.java
@@ -3,7 +3,6 @@ import java.time.LocalDate;
 public class Enseignant extends Utilisateur{
     Faculte fac;
     LocalDate annee;
-// Constructor
     public Enseignant(String nom, String prenom, String password, String mat,
                     double repC, double repP,LocalDate annee,Faculte fac)
             throws MatriculeException, ReputationException{
@@ -11,51 +10,38 @@ public class Enseignant extends Utilisateur{
         this.annee=annee;
         this.fac=fac;
     }
-// Getteurs
-    public Faculte getFaculteEn(int choix) {
-        System.out.println("Inserez votre Faculte: " +
-                "1)Math,\n" +
-                "2)Informatique,\n" +
-                "3)Physique,\n" +
-                "4)Chimie,\n" +
-                "5)Biologie,\n" +
-                "6)GénieC,\n" +
-                "7)GénieE,\n" +
-                "8)GénieMP,\n" +
-                "9)Géologie");
-
+    public LocalDate getAnnee(){
+        return this.annee;
+    }
+    public Faculte getFaculte(Faculte fac) {
+        return this.fac;
+    }
+    public Faculte setFaculteEn(int choix) {
         switch (choix) {
-            case 1: return Faculte.Math;
-            case 2: return Faculte.Informatique;
-            case 3: return Faculte.Physique;
-            case 4: return Faculte.Chimie;
-            case 5: return Faculte.Biologie;
-            case 6: return Faculte.GénieC;
-            case 7: return Faculte.GénieE;
-            case 8: return Faculte.GénieMP;
-            case 9: return Faculte.Géologie;
+            case 1: this.fac= Faculte.Math;
+            case 2: this.fac= Faculte.Informatique;
+            case 3: this.fac= Faculte.Physique;
+            case 4: this.fac= Faculte.Chimie;
+            case 5: this.fac= Faculte.Biologie;
+            case 6: this.fac= Faculte.GénieC;
+            case 7: this.fac= Faculte.GénieE;
+            case 8: this.fac= Faculte.GénieMP;
+            case 9: this.fac= Faculte.Géologie;
             default:
                 System.out.println("Erreur");
-                return null;
         }
-    }
-    public LocalDate getAnnee(){
-        return annee;
-    }
-// Setteurs
-    public void setFaculte(Faculte fac) {
-        this.fac = fac;
     }
 
     public void setAnnee(LocalDate annee) {
         this.annee = annee;
     }
-// Methode responsable d'inscription des enseignant dans l'app.
+
     public static void RajouterEnseignant(String filePath, Etudiant x) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath, true))) {
             out.writeObject(x);
+            System.out.println("Vous etes bien inscrit.");
         } catch (IOException e) {
-            System.err.println("ERREUR: " + e.getMessage());
+            System.err.println("Une erreur est survenue lors de l'écriture de l'objet : " + e.getMessage());
         }
     }
 }
