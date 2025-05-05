cc7c317d5b51fedce38803df42b1e7198f81eab6
meowgrabber <bwassim713@gmail.com>
2025-05-05 19:17:34 +0100

    Update Etudiant.java

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -4,7 +4,7 @@ public class Etudiant extends Utilisateur implements Serializable {
     LocalDate anneeAdmistion;
     Faculte fac;
     String specialite;
-// Constructeur
+
     public Etudiant(String nom, String prenom, String password, String mat,
                     double repC, double repP, LocalDate anneeAdmistion, Faculte fac, String specialite,)
             throws MatriculeException, ReputationException {
@@ -13,42 +13,40 @@ public class Etudiant extends Utilisateur implements Serializable {
         this.specialite = specialite;
         this.fac=fac;
     }
-    // Getteurs
     public String getSpecialite() {
-        return specialite;
+        return this.specialite;
     }
 
     public LocalDate getAnneeAdmistion() {
-        return anneeAdmistion;
+        return this.anneeAdmistion;
     }
-    public Faculte getFaculteEn(int choix) {
-        System.out.println("Inserez votre Faculte: " +
-                "1)Math,\n" +
-                "2)Informatique,\n" +
-                "3)Physique,\n" +
-                "4)Chimie,\n" +
-                "5)Biologie,\n" +
-                "6)GénieC,\n" +
-                "7)GénieE,\n" +
-                "8)GénieMP,\n" +
-                "9)Géologie");
-
-        switch (choix) {
-            case 1: return Faculte.Math;
-            case 2: return Faculte.Informatique;
-            case 3: return Faculte.Physique;
-            case 4: return Faculte.Chimie;
-            case 5: return Faculte.Biologie;
-            case 6: return Faculte.GénieC;
-            case 7: return Faculte.GénieE;
-            case 8: return Faculte.GénieMP;
-            case 9: return Faculte.Géologie;
-            default:
-                System.out.println("Erreur");
-                return null;
-        }
+    public Faculte getFaculte(){
+        return this.fac;
     }
-    // Setteurs
+    public void setFaculteEn(int choix) {
+                switch (choix) {
+                    case 1:
+                        this.fac = Faculte.Math;
+                    case 2:
+                        this.fac = Faculte.Informatique;
+                    case 3:
+                        this.fac = Faculte.Physique;
+                    case 4:
+                        this.fac = Faculte.Chimie;
+                    case 5:
+                        this.fac = Faculte.Biologie;
+                    case 6:
+                        this.fac = Faculte.GénieC;
+                    case 7:
+                        this.fac = Faculte.GénieE;
+                    case 8:
+                        this.fac = Faculte.GénieMP;
+                    case 9:
+                        this.fac = Faculte.Géologie;
+                    default:
+                        System.out.println("Erreur");
+                }
+    }
     public void setAnneeAdmistion(LocalDate anneeAdmistion) {
         this.anneeAdmistion = anneeAdmistion;
     }
@@ -60,7 +58,6 @@ public class Etudiant extends Utilisateur implements Serializable {
     public void setFac(Faculte fac) {
         this.fac = fac;
     }
-    // Methode responsable d'inscription des etudiants dans l'app.
     public static void RajouterEtudiant(String filePath, Etudiant x) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath, true))) {
             out.writeObject(x);
